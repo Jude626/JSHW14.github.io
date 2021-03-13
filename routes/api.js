45e65a62b6a5d0bf3aa97c8c3b500b27ad318f8e
Jude <jantungkuno@gmail.com>
2021-03-13 00:32:38 -0500

    completed post request for workout

--- routes/api.js
+++ routes/api.js
@@ -32,4 +32,11 @@ router.put("/api/workouts/:id", (req, res) => {
 });
 
 // Post request for creating workouts //
-router.post()
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body).then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(err => {
+        res.json(err);
+    });
+});
+
